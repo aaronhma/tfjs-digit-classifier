920dcdba221feac283dfb98e68b99902bd17c988
Aaron Ma <aaronhma@gmail.com>
2019-03-26 10:11:29 -0700

    write comments on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,26 +19,36 @@ export class AppComponent implements OnInit {
     this.loadModel();
   }
 
+  // Model stored at: https://github.com/aaronhma/tfjs-digit-classifier/tree/master/src/assets/cnn
+  // tf.js layers format is a directory containing a model.json file and a set of sharded weight files in binary format.
+  // model.json contains both the model topology (aka "architecture" or "graph": a description of the layers and how they are
+  // connected) and a manifest of the weight files.
   async loadModel() {
+    // Load a model that composed of layer objects
+    // Use TF.js converter to convert digit recognizer model to TF.js Layers format
     this.model = await tf.loadLayersModel('assets/cnn/model.json');
   }
 
   async predict(imageData: ImageData | null) {
+    // condition below is used when clear button is clicked
     if (imageData === null) {
       this.predictions = undefined;
       return;
     }
 
+    // use tidy function to help avoid memory leaks with automatic memory cleanup
     await tf.tidy(() => {
-      // Convert the canvas pixels to
+      // Convert the canvas pixels to TensorFlow Tensor
       let img = tf.browser.fromPixels(imageData, 1);
+      // Data preparation: remove the color dimension and normalizing pixel values
       img = img.reshape([1, 28, 28, 1] as any);
+      // Convert the tensor to a new type: float32
       img = tf.cast(img, 'float32');
 
-      // Make and format the predications
+      // Use pretrained model execute the inference for the input tensor (canvas pixel)
       const output = this.model.predict(img) as any;
 
-      // Save predictions on the component
+      // Save predictions to an output arary and accessed on the component
       this.predictions = Array.from(output.dataSync());
     });
   }
